655379d380afed7020b8fbbb3aee2340a5b7ee8f
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2024-03-11 03:23:21 -0700

    update: mongo, flask, rabbitmq

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -5,3 +5,17 @@ from auth import access
 from auth_svc import access
 from storage import util
 
+server = Flask(__name__)
+server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
+
+# Wrap flask server so we can manage mongoDB connections for flask app
+mongo = PyMongo(server)
+
+# GridFS wraps mongo db to enable working with files 
+# bigger than 16mb by breaking it up into chunks 
+# https://www.mongodb.com/docs/manual/core/gridfs/
+fs = gridfs.GridFS(mongo.db)
+
+
+# Configure RabbitMQ connection
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
